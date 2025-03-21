9711bfcf9b8074593f5d36b8f17d2609f410d805
Miguelsmaia <73429180+Miguelsmaia@users.noreply.github.com>
2025-03-21 11:48:21 +0000

    Added drivers info and stages/overall tables functions

--- rc_dashboard.py
+++ rc_dashboard.py
@@ -62,7 +62,7 @@ event = rally_dict[rally_name][0]
 ## STAGES
 
 def get_stages(url):
-    df = pd.DataFrame(json.loads(requests.get(url_stages).text))
+    df = pd.DataFrame(json.loads(requests.get(url).text))
     return {code: [stage_id, name, status] for code, stage_id, name, status in zip(df["code"], df["stageId"], df["name"], df["status"])}
 
 url_stages = f'https://p-p.redbull.com/rb-wrccom-lintegration-yv-prod/api/events/{event}/stages.json'
@@ -77,8 +77,59 @@ rally_stage = st.selectbox(
 
 stage_id = stage_dict[rally_stage][0]
 
+## GET DRIVERS INFO
 
+drivers = f'https://p-p.redbull.com/rb-wrccom-lintegration-yv-prod/api/events/{event}/rallies/{rally}/entries.json'
 
-## SPLIT TIMES
+def get_drivers(url):
+    df = pd.DataFrame(json.loads(requests.get(url).text))
+    df_clean = df.loc[df["groupId"] == 152, ["driver", "entryId"]]
+    df_clean["driver"] = df["driver"].apply(lambda x: x["abbvName"])
+    return {driver: entry_id for driver, entry_id in zip(df_clean["driver"], df_clean["entryId"])}, df_clean
 
+driver_dict, drivers_clean = get_drivers(drivers)
 
+## STAGE AND OVERALL TIMES
+
+def get_times(url_stage, url_overall):
+    df_stage = pd.DataFrame(json.loads(requests.get(url_stage).text))
+    df_overall = pd.DataFrame(json.loads(requests.get(url_overall).text))
+
+    # Cleaning overall table
+    # Transforming ms to seconds
+    df_overall[["diffFirstOverall", "diffPrevOverall"]] = df_overall[["diffFirstMs", "diffPrevMs"]] / 1000
+    # Cleaning time columns by removing "PT"
+    df_overall[["stageTime", "penaltyTime", "totalTime", "diffFirst", "diffPrev"]] = df_overall[["stageTime", "penaltyTime", "totalTime", "diffFirst", "diffPrev"]].replace("PT", "", regex=True)
+    # Creating columns with the stage (SS*)
+    df_overall["stage"] = stage
+    # Joining column with the name of the driver
+    df_overall = df_overall.merge(drivers_clean, on="entryId", how="left")
+    # Creating a new df with only necessary columns
+    df_overall_clean = df_overall[["driver", "position", "stageTime", "penaltyTime", "totalTime", "diffFirstOverall", "diffPrevOverall", "stage"]]
+
+    # Cleaning stage table
+    # Transforming ms to seconds
+    df_stage[["diffFirst", "diffPrev"]] = df_stage[["diffFirstMs", "diffPrevMs"]] / 1000
+    # Creating columns with the stage (SS*)
+    df_stage["stage"] = stage
+    # Joining column with the name of the driver
+    df_stage = df_stage.merge(drivers_clean, on="entryId", how="left")
+    df_stage = df_stage.merge(df_overall[["entryId", "diffFirstOverall"]], on="entryId", how="left")
+    # Creating a new df with only necessary columns
+    df_stage_clean = df_stage[["driver", "diffFirst", "diffPrev", "stage", "diffFirstOverall"]]
+
+    return df_stage_clean, df_overall_clean
+
+all_stages = pd.DataFrame()
+all_overall = pd.DataFrame()
+for stage, (stage_id, name, status) in stage_dict.items():
+    if status != "ToRun":
+        url_stage = f'https://p-p.redbull.com/rb-wrccom-lintegration-yv-prod/api/events/{event}/stages/{stage_id}/stagetimes.json?rallyId={rally}&championshipId=287&priority=P1'
+        url_overall = f'https://p-p.redbull.com/rb-wrccom-lintegration-yv-prod/api/events/{event}/stages/{stage_id}/results.json?rallyId={rally}&championshipId=287&priority=P1'
+        
+        df_stage_clean, df_overall_clean = get_times(url_stage, url_overall)
+            
+    all_stages = pd.concat((all_stages, df_stage_clean))
+    all_overall = pd.concat((all_overall, df_overall_clean))
+
+st.dataframe(all_stages[all_stages["stage"] == rally_stage])
