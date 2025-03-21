d8450dce0cd38be416ec334cc22b2b4cb81e9273
Miguelsmaia <73429180+Miguelsmaia@users.noreply.github.com>
2025-03-21 13:24:03 +0000

    Added markers to the charts

--- rc_dashboard.py
+++ rc_dashboard.py
@@ -154,10 +154,10 @@ tab3, tab4 = st.tabs(["Stage time", "Overall"])
 
 with tab3:
     st.header("Stage chart")
-    fig = px.line(all_stages, x="stage", y="diffFirst", color="driver")
+    fig = px.line(all_stages, x="stage", y="diffFirst", color="driver", markers=True)
 
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
 with tab4:
-    fig2 = px.line(all_overall, x="stage", y="diffFirstOverall", color="driver")
+    fig2 = px.line(all_overall, x="stage", y="diffFirstOverall", color="driver", markers=True)
 
     st.plotly_chart(fig2, theme="streamlit", use_container_width=True)
